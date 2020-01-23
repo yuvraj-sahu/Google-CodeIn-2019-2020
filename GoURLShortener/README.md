cb0ec14147edc44ead9569846ddce4595bc843a8
Yuvraj <ysahu.atx@gmail.com>
2020-01-22 22:57:55 -0600

    Added map and descriptions to GoURLShortener

--- GoURLShortener/README.md
+++ GoURLShortener/README.md
@@ -1,3 +1,3 @@
 # Go URL Shortener
 
-To use, you can simply enter 'page' into your command prompt, since page.go has already been compiled. If you would like to change something, simply change what you want, recompile using 'go build page.go' (make sure that you have go installed), and run 'page'.
+To use, you can simply enter 'page' into your command prompt, since page.go has already been compiled. If you would like to change something, simply change what you want, recompile using 'go build page.go' (make sure that you have go installed), and run 'page'. Press Ctrl + C to stop the program.
--- GoURLShortener/page.go
+++ GoURLShortener/page.go
@@ -6,16 +6,24 @@ import (
     "fmt"
 )
 
+var redirectionMap map[string]string
+
 func handler(w http.ResponseWriter, r *http.Request) {
     path := r.URL.Path
-    if path == "/go" {
-        http.Redirect(w, r, "http://golang.org", http.StatusFound)
+    redirectionURL := redirectionMap[path]
+    if redirectionURL == "" {
+        fmt.Fprintf(w, "Hello! Go to localhost:8080/go to be redirected to golang.org, or go to localhost:8080/google to be redirected to google.com. ")
     } else {
-        fmt.Fprintf(w, "Hello! Go to localhost:8080/go to be redirected to golang.org. ")
+        http.Redirect(w, r, redirectionURL, http.StatusFound)
     }
 }
 
 func main() {
+
+    redirectionMap = make(map[string]string)
+    redirectionMap["/go"] = "http://golang.org"
+    redirectionMap["/google"] = "http://google.com"
+
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
